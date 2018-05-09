487d92e92637da47a5d6d7bd211c021327699008
ZF <306484640@qq.com>
2018-05-09 14:16:31 +0800

    tianqu

---
+++ tianqi/src/tianqi/Test.java
@@ -0,0 +1,10 @@
+package tianqi;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		Ther restUtil  = new Ther();
+		restUtil.interfaceUtil("https://image.baidu.com/search/index?tn=baiduimage&ct=201326592&lm=-1&cl=2&ie=gbk&word=%D4%AD%D0%CD%CD%BC&fr=ala&ala=1&alatpl=adress&pos=0&hs=2&xthttps=111111");
+	}
+
+}
