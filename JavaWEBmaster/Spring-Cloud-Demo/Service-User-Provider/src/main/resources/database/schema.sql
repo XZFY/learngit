b093d62b5ce2dbd670f0330f5e02675dc079462b
ZF <306484640@qq.com>
2018-05-30 17:43:08 +0800

    删除全部文件

--- JavaWEBmaster/Spring-Cloud-Demo/Service-User-Provider/src/main/resources/database/schema.sql
+++
@@ -1,6 +0,0 @@
-drop table user if exists;
-create table user (
-  id INT(11) AUTO_INCREMENT PRIMARY KEY ,
-  username VARCHAR(255),
-  password VARCHAR(255)
-  );
--- ZFFF/src/main/java/com/zf/ZFFF/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zf.ZFFF.mapper;
-
-import java.util.List;
-
-import com.zf.ZFFF.entity.User;
-
-public interface UserMapper {
-	public List<User> find();
-}
